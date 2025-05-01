6f15ad9a02f8d0cb0980f626add7dc87836e0b2d
ishanrtripathi786 <ishanrtripathi786@gmail.com>
2025-05-01 18:06:14 +0000

    downgrade node

--- src/server/server.js
+++ src/server/server.js
@@ -116,7 +116,7 @@ const startServer = async () => {
         await connectDB();
         logger.info('Database connected successfully');
         logger.info(`Attempting to start server listener on port ${PORT}...`); // Added log
-        app.listen(PORT, () => {
+        app.listen(PORT, 0.0.0.0, () => {
             logger.info(`Server running on port ${PORT}`);
         });
     } catch (err) {
